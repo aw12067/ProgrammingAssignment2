f3797f072cdce1d48e5068185a0faae1c7d7a1ad
Ashwin Walunj <52004371+aw12067@users.noreply.github.com>
2020-08-06 18:26:59 +0530

    10 commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,34 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix())
-{
-           m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+makeCacheMatrix <- function(x = matrix()) {
+                inv <- NULL
+                set <- function(y) {
+                  x <<- y
+                  inv <<- NULL
+                }
+                get <- function() x
+                setInverse <- function(inverse) inv <<- inverse
+                getInverse <- function() inv
+                list(set = set,
+                     get = get,
+                     setInverse = setInverse,
+                     getInverse = getInverse)
 }
 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) 
-{
+cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-          m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
+                inv <- x$getInverse()
+                if (!is.null(inv)) {
+                  message("getting cached matrix")
+                  return(inv)
+                }
+                mat <- x$get()
+                inv <- solve(mat, ...)
+                x$setInverse(inv)
+                inv
 }
